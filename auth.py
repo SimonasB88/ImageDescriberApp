1664fbcdb740b71719850895044c2f0226f64160
SimonasB88 <simonas.burneikis@gmail.com>
2024-11-25 20:08:43 +0200

    small fixes

--- auth.py
+++ auth.py
@@ -12,9 +12,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.date() + expires_delta
+        expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.date() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
